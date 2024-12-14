1534577abe1e87dc1fcd2d52eaa937d70c29f35a
ayush_96 <ayush.sharma@student.torrens.edu.au>
2024-12-14 19:30:14 +1030

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+createRoot(document.getElementById('root')).render(
+  <Router>
+  <StrictMode>
+    <App />
+  </StrictMode>
+  </Router>,
+)
